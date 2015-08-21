77c7e846ba9cc7823737ea8c2d2e0a77c96d0141
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-08-21 07:41:01 -0700

    rename script outline

---
+++ scripts/file_rename.py
@@ -0,0 +1,12 @@
+import os
+
+# code outline
+# get directory structure
+# care because windows machines have \\ instead of //
+# get names of lowest level directory
+# first split will be at date
+# for loop to find files with XXX_output.text
+# copy these files to new directory appending date to begining of name
+# assume the directory stucture is the same on all machines?
+
+# see http://stackoverflow.com/questions/120656/directory-listing-in-python
